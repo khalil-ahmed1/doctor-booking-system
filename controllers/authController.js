feeec850e5f2363ef59e6df581552751871c1587
khalil ahmed <ka8932007@gmail.com>
2026-06-14 11:30:22 +0530

    added hashed password

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,5 +1,5 @@
 const User = require("../models/User");
-
+const bcrypt = require("bcryptjs");
 const registerUser = async (req, res) => {
   try {
     const { name, mobile, email, password } = req.body;
@@ -13,12 +13,14 @@ const registerUser = async (req, res) => {
         message: "Mobile number already registered",
       });
     }
+const salt = await bcrypt.genSalt(10);
 
+const hashedPassword = await bcrypt.hash(password, salt);
     const user = await User.create({
       name,
       mobile,
       email,
-      password,
+      password: hashedPassword,
     });
 
     res.status(201).json({
