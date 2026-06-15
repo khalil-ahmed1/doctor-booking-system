b5cf891b7a66e12aa84ecc18967f75d05b487be9
khalil ahmed <ka8932007@gmail.com>
2026-06-15 10:34:27 +0530

    Added doctor listing and profile APIs

--- controllers/doctorController.js
+++ controllers/doctorController.js
@@ -40,7 +40,48 @@ const registerDoctor = async (req, res) => {
     });
   }
 };
+const getAllDoctors = async (req, res) => {
+  try {
+    const doctors = await Doctor.find();
+
+    res.status(200).json({
+      success: true,
+      count: doctors.length,
+      doctors,
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
+
+const getDoctorById = async (req, res) => {
+  try {
+    const doctor = await Doctor.findById(req.params.id);
+
+    if (!doctor) {
+      return res.status(404).json({
+        success: false,
+        message: "Doctor not found",
+      });
+    }
+
+    res.status(200).json({
+      success: true,
+      doctor,
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
 
 module.exports = {
   registerDoctor,
+  getAllDoctors,
+  getDoctorById,
 };
--- routes/doctorRoutes.js
+++ routes/doctorRoutes.js
@@ -2,8 +2,16 @@ const express = require("express");
 
 const router = express.Router();
 
-const { registerDoctor } = require("../controllers/doctorController");
+const {
+  registerDoctor,
+  getAllDoctors,
+  getDoctorById,
+} = require("../controllers/doctorController");
 
 router.post("/register", registerDoctor);
+
+router.get("/", getAllDoctors);
+
+router.get("/:id", getDoctorById);
 
 module.exports = router;
